c46ae21c5e6fbb58a9b3542c1d11aed033554339
harryyking <arthurharry06@gmail.com>
2025-03-22 17:29:32 +0000

    auth pge

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function AuthPage() {
   const { data: session, status } = useSession();
+  const [googleIsLoading, setGoogleIsLoading] = useState(false)
   const router = useRouter();
 
   if (status === "loading") {
-    return <div className="text-center p-4">Loading...</div>;
+    return <div className="text-center p-4 min-h-screen">Loading...</div>;
   }
 
   if (session) {
@@ -15,13 +17,26 @@ export default function AuthPage() {
     return null;
   }
 
+  const handleSignInGoogle = async() => {
+    setGoogleIsLoading(true);
+    const signInResult = await signIn("google", {callbackUrl: '/dashboard'});
+    if (!signInResult) {
+      setGoogleIsLoading(false);
+      return;
+    }
+
+    if(signInResult.ok) {
+      router.push("/dashboard");
+    }
+  }
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-base-200 flex items-center justify-center p-4">
       <div className="card bg-white shadow-md p-6">
         <h1 className="text-2xl font-bold mb-4">Sign In</h1>
         <button
           className="btn btn-primary w-full"
-          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          onClick={handleSignInGoogle}
         >
           Sign In with Google
         </button>
