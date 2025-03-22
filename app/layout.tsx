5390642fcc7a90fa46ac8d78e6b5e33384f9e465
harryyking <arthurharry06@gmail.com>
2025-03-22 11:08:22 +0000

    apply theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="emerald">
       <body>
         {children}
         <Toaster position="top-right" />
